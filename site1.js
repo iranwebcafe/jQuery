8cc40f87f27d4cff648f2e7d806381277df95921
iranwebcafe <36415738+iranwebcafe@users.noreply.github.com>
2018-07-16 11:47:10 +0430

    JQuery Source  Code

---
+++ site1.js
@@ -0,0 +1,6 @@
+﻿function display() {
+		    		document.write("سلام ، دکتر رضا زعفرانیه هستم ");
+					document.write("<br>");
+					document.write("به وبگاه من خوش آمدید ");
+  }  //  end of display function
+	
